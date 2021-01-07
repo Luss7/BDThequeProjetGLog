f7479652b2907eb830177c395121345b0060afa5
OCaracatsanis <47111648+OCaracatsanis@users.noreply.github.com>
2021-01-07 16:38:53 +0100

    Création des repository, début de restructuration des forms

--- App/Accueil.cs
+++ App/Accueil.cs
@@ -5,19 +5,65 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using DAL;
+using Domain;
 
 namespace App
 {
     public partial class Accueil : Form
     {
+        private IAllAlbumsRepository tousLesAlbumsRepository;
+        private IMyAlbumsRepository mesAlbumsRepository;
+        private IWishlistRepository wishlistRepository;
+
         public Accueil()
         {
             InitializeComponent();
         }
 
+        private void AfficherContenu()
+        {
+           /* try
+            {
+                dgvUtils.Rows.Clear();  // suppression des éventuelles lignes existantes
+                // Accès à la liste des utilisateurs et remplissage du tableau
+                foreach (Utilisateur utilisateur in utilisateurRepository.GetAll())
+                {
+                    dgvUtils.Rows.Add(utilisateur.Decrire());
+                }
+                // tri sur la 1ère colonne (nom)
+                dgvUtils.Sort(dgvUtils.Columns[0], ListSortDirection.Ascending);
 
+                dgvLivres.Rows.Clear();  // suppression des éventuelles lignes existantes
+                // Accès à la liste des livres et remplissage du tableau
+                foreach (Livre livre in livreRepository.GetAll())
+                {
+                    dgvLivres.Rows.Add(livre.Decrire());
+                }
+                // tri sur la 1ère colonne (ISBN)
+                dgvLivres.Sort(dgvLivres.Columns[0], ListSortDirection.Ascending);
+
+                dgvEmprunts.Rows.Clear();  // suppression des éventuelles lignes existantes
+                // Accès à la liste des emprunts et remplissage du tableau
+                foreach (Emprunt emprunt in empruntRepository.GetAll())
+                {
+                    string date = emprunt.Date.ToShortDateString();
+                    string emprunteur = emprunt.Emprunteur.ToString();
+                    string livre = emprunt.Livre.Titre;
+                    string rendu = emprunt.Rendu.ToString();
+
+                    string[] ligne = { date, emprunteur, livre, rendu };
+                    dgvEmprunts.Rows.Add(ligne);
+                }
+                // tri sur la 1ère colonne (date de l'emprunt)
+                dgvEmprunts.Sort(dgvEmprunts.Columns[0], ListSortDirection.Descending);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }*/
 
         private void btn_MyAlbums_Click(object sender, EventArgs e)
         {
@@ -46,7 +92,7 @@ namespace App
 
         private void link_Deconnexion_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-
+            Application.Exit();
         }
     }
 }
---
+++ DAL/IWishlistRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace DAL
+{
+    public interface IWishlistRepository
+    {
+        //TODO : Récupère la liste des albums de la wishlist
+        IList<WishList> GetAll();
+    }
+}
